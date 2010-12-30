23cd578a44418c70f55ddf078724c56784f80c8a
Kipras <kiprasm@gmail.com>
2010-12-30 10:02:14 +0200

    Updated release notes

--- tests/1.x.version_to_test.php
+++ tests/1.x.version_to_test.php
@@ -1,1 +1,9 @@
-<?php// Hydrate version to test// Hydrate.class.php        : points to the latest stable version// Hydrate.class.trunk.php  : is the current working filerequire_once(dirname(__FILE__) . '/../Hydrate.class.php');// require_once(dirname(__FILE__) . '/../Hydrate.class.1.10.php');
+<?php
+
+// Hydrate version to test
+// Hydrate.class.php        : points to the latest stable version
+// Hydrate.class.trunk.php  : is the current working file
+
+
+require_once(dirname(__FILE__) . '/../Hydrate.class.php');
+// require_once(dirname(__FILE__) . '/../Hydrate.class.1.11.php');
